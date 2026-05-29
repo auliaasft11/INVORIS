9608ade9ddc2cf7b766e7b49ffc843850703224f
lailatummusfiroh <laelatummusfiroh05@gmail.com>
2026-05-29 20:09:10 +0700

    menambahkan fitur autentikasi dan role user

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Redirect setelah register
+     *
+     * @var string
+     */
+    protected $redirectTo = '/login';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Logout setelah register
+     */
+    protected function registered($request, $user)
+    {
+        auth()->logout();
+    }
+
+    /**
+     * Validator register
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create user
+     *
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\PeminjamanController;
 
+// Langsung ke Login saat pertama buka
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+Auth::routes();
+
+// Semua halaman di bawah ini wajib Login
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home',
+        [HomeController::class, 'index'])
+        ->name('home');
+
+    // Rute Data Barang
+    Route::get('/barang',
+        [BarangController::class, 'index'])
+        ->name('barang.index');
+
+    Route::post('/barang',
+        [BarangController::class, 'store'])
+        ->name('barang.store');
+
+    Route::delete('/barang/{id}',
+        [BarangController::class, 'destroy'])
+        ->name('barang.destroy');
+
+    // Rute Peminjaman
+    Route::get('/peminjaman',
+        [PeminjamanController::class, 'index'])
+        ->name('peminjaman.index');
+
+    Route::post('/peminjaman',
+        [PeminjamanController::class, 'store'])
+        ->name('peminjaman.store');
+
+    // SELESAIKAN PEMINJAMAN
+    Route::put('/peminjaman/{id}/selesai',
+        [PeminjamanController::class, 'selesai'])
+        ->name('peminjaman.selesai');
+
 });
