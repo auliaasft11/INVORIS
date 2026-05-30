67d9c3e84e0155fd131b61df9b87fe3775ee6ddd
syalwa <syalwaalya42@gmail>
2026-05-30 15:42:34 +0700

    Memperbaiki seeder data barang

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -10,15 +10,21 @@ class BarangSeeder extends Seeder
     public function run(): void
     {
         Barang::create([
+            'kode_barang' => 'BRG-001',
             'nama_barang' => 'HT',
+            'kategori' => 'Elektronik',
             'stok' => 3,
             'kondisi' => 'Ready',
+            'lokasi' => 'Gudang',
         ]);
 
         Barang::create([
+            'kode_barang' => 'BRG-002',
             'nama_barang' => 'Printer Epson',
+            'kategori' => 'Elektronik',
             'stok' => 5,
             'kondisi' => 'Ready',
+            'lokasi' => 'Gudang',
         ]);
     }
 }
