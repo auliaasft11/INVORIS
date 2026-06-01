0c639c99d388b23c65fc2801b744365a447f41cb
auliaasft11 <nraulsftri11@gmail.com>
2026-06-01 17:55:24 +0800

    Menghapus field yang tidak digunakan pada tabel barang

--- database/migrations/2026_05_29_093822_create_barangs_table.php
+++ database/migrations/2026_05_29_093822_create_barangs_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('kode_barang')->unique();
             $table->string('nama_barang');
             $table->string('kategori');
             $table->integer('stok');
             $table->string('kondisi');
-            $table->string('lokasi');
-            $table->string('gambar_barang')->nullable();
+            $table->timestamps();
+
         });
     }
 
